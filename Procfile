5f3072656c8df2b945cfba0a9f7db96e551f912a
Jace Browning <jacebrowning@gmail.com>
2015-10-17 21:05:50 -0400

    Add link back to GitHub organization

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn hello:app
+web: gunicorn --reload hello:app
--- hello.py
+++ hello.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return "Hello, World!"
+    return "Please visit: https://github.com/votestakes"
 
 
 if __name__ == '__main__':
